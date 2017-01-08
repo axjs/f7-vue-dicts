648eed4d9648d8b7f160fde7f433f6a7fe97b1f2
ax <ax@ax.ax>
2017-01-08 21:45:28 +0200

    auth

--- src/login.vue
+++ src/login.vue
@@ -5,11 +5,11 @@
     <f7-view>
       <f7-pages>
         <f7-page login-screen>
-          <f7-login-screen-title>Login + {{ user ? user.uid + '/' + user.isAnonymous : '-'}}</f7-login-screen-title>
-          <f7-list form>
+          <f7-login-screen-title v-text="name"></f7-login-screen-title>
+          <f7-list form v-if="!user">
             <f7-list-item>
               <f7-label>Username</f7-label>
-              <f7-input name="username" placeholder="Username" type="text"></f7-input>
+              <f7-input name="username" placeholder="Username" type="text" autofocus></f7-input>
             </f7-list-item>
             <f7-list-item>
               <f7-label>Password</f7-label>
@@ -17,11 +17,13 @@
             </f7-list-item>
           </f7-list>
           <f7-list>
-            <f7-list-button title="Sign In" close-login-screen></f7-list-button>
-            <f7-list-label>
+            <f7-list-button v-if="!user" title="Sign In" close-login-screen></f7-list-button>
+            <f7-list-label v-if="!user">
               <p>Click Sign In to close Login Screen</p>
             </f7-list-label>
-            <f7-list-button title="toggleSignIn" @click="toggleSignIn"></f7-list-button>
+            <f7-list-button v-if="!user" title="Guest sign in" @click="signInAnonymously" close-login-screen></f7-list-button>
+            <f7-list-button v-if="user" title="Sign Out" @click="signOut" close-login-screen></f7-list-button>
+            <!--<f7-list-button title="toggleSignIn" @click="toggleSignIn"></f7-list-button>-->
           </f7-list>
         </f7-page>
       </f7-pages>
@@ -32,7 +34,7 @@
 
 <script>
 
-  import firebase from './fb.js' 
+  import firebase from './fb.js'
 
   export default {
     data: function () {
@@ -40,28 +42,55 @@
         user: null,
       };
     },
+    computed: {
+      name: function () {
+        var name = 'Not login'
+        if (this.user) {
+          if (this.user.isAnonymous) {
+            name = 'guest'
+          } else {
+            name = this.user.uid
+          }
+        }
+        return name
+      }
+    },
+
     methods: {
+      signOut: function () {
+        if (firebase.auth().currentUser) {
+          firebase.auth().signOut();
+        }
+      },
+      signInAnonymously: function () {
+        if (firebase.auth().currentUser) {
+          alert('Already signed')
+          return;
+        }
+        firebase.auth().signInAnonymously().catch(function (error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          if (errorCode === 'auth/operation-not-allowed') {
+            alert('You must enable Anonymous auth in the Firebase Console.');
+          } else {
+            console.error(error);
+          }
+        });
+      },
       toggleSignIn: function () {
         console.log('toggleSignIn')
         if (firebase.auth().currentUser) {
-          // [START signout]
           firebase.auth().signOut();
-          // [END signout]
         } else {
-          // [START authanon]
           firebase.auth().signInAnonymously().catch(function (error) {
-            // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
-            // [START_EXCLUDE]
             if (errorCode === 'auth/operation-not-allowed') {
               alert('You must enable Anonymous auth in the Firebase Console.');
             } else {
               console.error(error);
             }
-            // [END_EXCLUDE]
           });
-          // [END authanon]
         }
         // document.getElementById('quickstart-sign-in').disabled = true;
       }
